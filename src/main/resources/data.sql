d8a6b6bc55d5e419130dfaf0f2d9c1d120a5a0ab
Grigoryev Pavel <Green-ono@yandex.ru>
2022-12-14 23:49:43 +0300

    added liquibase and data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO cash_receipt.public.product (name, price, promotion, quantity, total)
+VALUES ('Перфоратор Bosh', 575.25, true, 1, 575.25);
+INSERT INTO cash_receipt.public.product (name, price, promotion, quantity, total)
+VALUES ('Самовар золотой', 100.99, false, 1, 100.99);
+INSERT INTO cash_receipt.public.product (name, price, promotion, quantity, total)
+VALUES ('Перчатки шерсть', 30.89, true, 1, 30.89);
+INSERT INTO cash_receipt.public.product (name, price, promotion, quantity, total)
+VALUES ('Лапша съедобная', 7.40, true, 1, 7.40);
+INSERT INTO cash_receipt.public.product (name, price, promotion, quantity, total)
+VALUES ('Люк ревизионный', 50.73, true, 1, 50.73);
+
+INSERT INTO cash_receipt.public.discount_card (discount_card_number, discount_percentage)
+VALUES ('5678', 5);
+INSERT INTO cash_receipt.public.discount_card (discount_card_number, discount_percentage)
+VALUES ('1234', 3);
+INSERT INTO cash_receipt.public.discount_card (discount_card_number, discount_percentage)
+VALUES ('9875', 10);
